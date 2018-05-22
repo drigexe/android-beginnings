abb9792a8433426d5cf1ec90bf8c634c4612cb52
Yuri Vysocki <drigexe@gmail.com>
2018-05-22 17:04:30 +0300

    variation of in-app custom keyboard

--- android-beginnings/they-see-me-rolling/app/src/main/java/com/vysocki/yuri/theyseemerolling/MainActivity.java
+++ android-beginnings/they-see-me-rolling/app/src/main/java/com/vysocki/yuri/theyseemerolling/MainActivity.java
@@ -2,6 +2,10 @@ package com.vysocki.yuri.theyseemerolling;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        MyKeyboard keyboard = (MyKeyboard) findViewById(R.id.keyboard);
+
+        // prevent system keyboard from appearing when EditText is tapped
+        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        editText.setTextIsSelectable(true);
+
+        // pass the InputConnection from the EditText to the keyboard
+        InputConnection ic = editText.onCreateInputConnection(new EditorInfo());
+        keyboard.setInputConnection(ic);
     }
+
 }
