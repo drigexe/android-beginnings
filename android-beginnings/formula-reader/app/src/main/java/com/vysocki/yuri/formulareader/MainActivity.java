da6b7f143c39a67bea456356715a338c5b4d1650
Yuri Vysocki <drigexe@gmail.com>
2018-05-24 19:44:33 +0300

    formulaSeparator is working correctly

--- android-beginnings/formula-reader/app/src/main/java/com/vysocki/yuri/formulareader/MainActivity.java
+++ android-beginnings/formula-reader/app/src/main/java/com/vysocki/yuri/formulareader/MainActivity.java
@@ -2,6 +2,7 @@ package com.vysocki.yuri.formulareader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,15 +17,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
-        String myFinalResult = calculateFormula("2d10+8-1d4+100d8-7");
-        textView.setText(myFinalResult);
+        //String myFinalResult = calculateFormula("2d10+8-1d4+100d8-7");
+        //textView.setText(myFinalResult);
+
+        //ArrayList<String> asd = new ArrayList<>();   <- to see if formulaSeparator is working
+        //asd = formulaSeparator("2d10+8-1d4+100d8-7");  <- to see if formulaSeparator is working
     }
 
     public ArrayList<String> formulaSeparator(String formula) {
         ArrayList<String> formulaStringParts = new ArrayList<>();
+        char plus = '+';
+        char minus = '-';
+        int pos1 = 0;
+        int pos2;
 
         //separate formula into the individual string parts
         //and put them into string array
+
+        for (int i = 0; i < formula.length(); i++) {
+            if ((formula.charAt(i) == plus) || (formula.charAt(i) == minus)) {
+                pos2 = i;
+                formulaStringParts.add(formula.substring(pos1, pos2));
+                pos1 = i+1;
+            }
+        }
+
+        pos2 = formula.length();
+        formulaStringParts.add(formula.substring(pos1, pos2));
+
+        //for (int e = 0; e < formulaStringParts.size(); e++ ) {
+        //   Log.i("MASSIV", formulaStringParts.get(e));
+        //}
 
         return formulaStringParts;
     }
@@ -70,3 +93,7 @@ public class MainActivity extends AppCompatActivity {
         return sResult;
     }
 }
+
+// indexOf(String str, int fromIndex)
+// substring(int start, int end)
+// charAt(int index)
